a90c8fc0f81fec5bd0a70567234d15b225868edc
Chiu Z. Yang <chiuup@gmail.com>
2018-01-27 23:14:32 -0800

    dude

---
+++ Assets/SomeDude.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SomeDude : MonoBehaviour
+{
+    public Camera cam;
+    public float speed = 0.01f;
+    public int minStep = 10;
+    public int maxStep = 50;
+
+    private bool rolled = false;
+    private int dir = 0;
+    private int steps = 0;
+    private Vector3[] dirVector = new Vector3[] { Vector3.left, Vector3.right, Vector3.up, Vector3.down, Vector3.zero};
+    // Use this for initialization
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 vp = cam.WorldToViewportPoint(transform.position);
+
+        if (!rolled)
+        {
+            this.dir = Random.Range(0, dirVector.Length);
+            this.steps = Random.Range(minStep, maxStep);
+            rolled = true;
+        }
+        
+        vp += speed * dirVector[dir];
+        vp.x = Mathf.Clamp01(vp.x);
+        vp.y = Mathf.Clamp(vp.y, 1f-0.618f, 1f);
+        transform.position = cam.ViewportToWorldPoint(vp);
+
+        this.steps -= 1;
+        if ((steps) == 0)
+        {
+            rolled = false;
+        }
+    }
+}
---
+++ Assets/SomeFlip.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SomeFlip : MonoBehaviour {
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Time.frameCount % 10 == 0)
+        {
+            GetComponent<SpriteRenderer>().flipX = !GetComponent<SpriteRenderer>().flipX;
+        }
+    }
+}
